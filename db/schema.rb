c37e2a6d1b57b49991e8e1c29416e49c275dc292
delphine vilaysack <dvilaysack@keley-live.com>
2016-12-12 18:51:29 +0100

    Add nested form choices for survey

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212133552) do
+ActiveRecord::Schema.define(version: 20161212163720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "choices", force: :cascade do |t|
+    t.integer  "survey_id",  default: 0
+    t.string   "title"
+    t.integer  "order"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["survey_id"], name: "index_choices_on_survey_id", using: :btree
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.string   "title"
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 20161212133552) do
     t.datetime "updated_at",                          null: false
   end
 
+  add_foreign_key "choices", "surveys"
   add_foreign_key "surveys", "users", column: "creator_id"
 end
