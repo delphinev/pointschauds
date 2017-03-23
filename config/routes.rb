7f0545e7659cc9eb1b59c928010425d3eb30867a
delphine vilaysack <dvilaysack@keley-live.com>
2017-03-23 10:49:51 +0100

    Add gem localtower

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :surveys
   root to: "home#index"
+
+  if Rails.env.development?
+    mount Localtower::Engine, at: "localtower"
+  end
 end
